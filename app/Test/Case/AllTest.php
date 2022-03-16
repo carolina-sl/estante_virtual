68071458933796540f7441f5f2e09b9f3d62d68e
Joao Bittencourt <joaogsbittencourt@gmail.com>
2022-03-15 21:56:43 -0300

    improvements

--- app/Test/Case/AllTest.php
+++ app/Test/Case/AllTest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * AllModelTest
  *
@@ -8,18 +9,14 @@
  */
 class AllTest extends PHPUnit_Framework_TestSuite {
 
-/**
- * suite method, defines tests for this suite.
- *
- * @return void
- */
-	public static function suite() {
-		$suite = new CakeTestSuite('All Model related class tests');
-		$suite->addTestDirectory(__DIR__ . DS . 'Controller' . DS);
-		$suite->addTestDirectory(__DIR__ . DS . 'Controller' . DS . 'Component' . DS);
-		$suite->addTestDirectory(__DIR__ . DS . 'Model' . DS);
-		$suite->addTestDirectory(__DIR__ . DS . 'Model' . DS . 'Behavior' . DS);
-		$suite->addTestDirectory(__DIR__ . DS . 'View' . DS . 'Helper' . DS);
-		return $suite;
-	}
+    public static function suite() {
+        $suite = new CakeTestSuite('All Model related class tests');
+        $suite->addTestDirectory(__DIR__ . DS . 'Controller' . DS);
+        $suite->addTestDirectory(__DIR__ . DS . 'Controller' . DS . 'Component' . DS);
+        $suite->addTestDirectory(__DIR__ . DS . 'Model' . DS);
+        $suite->addTestDirectory(__DIR__ . DS . 'Model' . DS . 'Behavior' . DS);
+        $suite->addTestDirectory(__DIR__ . DS . 'View' . DS . 'Helper' . DS);
+        return $suite;
+    }
+
 }
